c5adde861c5b4f08c32db596e1adc564c396e130
md. @crypto <105918255+Hqzdev@users.noreply.github.com>
2026-09-23 13:15:40 +0500

    Initial Commit

---
+++ Somna/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Somna
+//
+//  Created by Yaroslav Fairfieldd on 9/23/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Somna/SomnaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SomnaApp.swift
+//  Somna
+//
+//  Created by Yaroslav Fairfieldd on 9/23/26.
+//
+
+import SwiftUI
+
+@main
+struct SomnaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
